fdc2a6e605b496627395fa8b770e3831d47df3f9
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2022-12-31 11:38:24 +0530

    Create Q-01. Evaluate Reverse Polish notation.java

---
+++ Microsoft/Q-01. Evaluate Reverse Polish notation.java
@@ -0,0 +1,39 @@
+LeetCode Problem Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<String> st = new Stack<>();
+
+        for(int i = 0; i < tokens.length; i++){
+            if(isOperator(tokens[i])){
+                int op2 = Integer.parseInt(st.pop()); 
+                int op1 = Integer.parseInt(st.pop()); 
+                String ans = operate(op1, op2, tokens[i]);
+                st.push(ans);
+            }else{
+                st.push(tokens[i]);
+            }
+        }
+        return Integer.parseInt(st.pop());
+    }
+
+    public boolean isOperator(String op){
+        return op.equals("+") || op.equals("*") || op.equals("/") || op.equals("-");
+    }
+
+    public String operate(int op1, int op2, String OP){
+        char op = OP.charAt(0);
+        int ans = 0;
+        if(op == '+'){
+            ans = op1 + op2;
+        }else if(op == '-'){
+            ans = op1 - op2;
+        }else if(op == '*'){
+            ans = op1 * op2;
+        }else if(op == '/'){
+            ans = op1 / op2;
+        }
+
+        return ans+"";
+    }
+}
