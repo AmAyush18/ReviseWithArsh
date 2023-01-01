eb6d2a8bf56585f4fcb455996beb214c6ad5eb49
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2023-01-01 20:26:12 +0530

    Update Q-02. Bulls and Cows.java

--- Microsoft/Q-02. Bulls and Cows.java
+++ Microsoft/Q-02. Bulls and Cows.java
@@ -1,5 +1,7 @@
 // LeetCode Problem Link: https://leetcode.com/problems/bulls-and-cows/description/
 
+// Approach - 1 
+// T.C: O(N) | S.C: O(N)
 class Solution {
     public String getHint(String secret, String guess) {
       
@@ -24,5 +26,36 @@ class Solution {
         String ans = bulls + "A" + cows + "B";
 
         return ans;
+    }
+}
+
+// Approach - 2
+// T.C: O(N) | S.C: O(1)
+class Solution {
+    public String getHint(String secret, String guess) {
+        
+        int bulls = 0, cows = 0;
+        // to keep track of all the numbers;
+        int[] track = new int[10];
+
+        for(int i = 0; i < secret.length(); i++){
+            int a = secret.charAt(i)-'0';
+            int b = guess.charAt(i)-'0';
+            // if both numbers are different
+            // means they are either misplaced or extra number
+            if(a != b){
+                // if track[a] < 0 means it was guessed earlier but at wrong place
+                if(track[a] < 0) cows++;
+                // if track[b] > 0 means the number b is present in secret number but at different place
+                if(track[b] > 0) cows++;
+
+                track[a]++; // increment the current number found in secret
+                track[b]--; // decrement the current number guessed
+            }
+            // if a and b are same means bulls++
+            else bulls++;
+        }
+
+        return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B";
     }
 }
